adbccacf561340313733a5bdbae21db9c55dfae2
Abrupaul0510 <58134241+Abrupaul0510@users.noreply.github.com>
2020-10-10 02:36:16 +0800

    Second with CTS

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 
 const { Client, DiscordAPIError } = require('discord.js');
+
+const puppeteer = require('puppeteer-extra')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+
 const axios = require('axios');
 const querystring = require('querystring');
 const Discord = require('discord.js');
@@ -88,9 +97,54 @@ client.on('message' , (message)=>{
                 message.channel.send('Item not found mister');
             });
           
+        }else if (CMD_NAME === 'cts'){
+
+            puppeteer.launch({ headless: true }).then(async browser => {
+                message.channel.send('Gathering Data From DFP....');
+                const page = await browser.newPage()
+                
+                message.channel.send('May take 10sec');
+                await page.goto('https://www.dfprofiler.com/clan/weekly-ts',{timeout: 0, waitUntil: 'networkidle0'})
+                message.channel.send('Almost there....');
+                const result = await page.$$eval('#DataTables_Table_0 tr', rows => {
+                  return Array.from(rows, row => {
+                    const columns = row.querySelectorAll('td');
+                    return Array.from(columns, column => column.innerText);
+                  });
+                });
+                message.channel.send('Gotcha...!');
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#3c00ff')
+                .setTitle('Top 5 - Weekly Clan Top Survivors')
+                
+                .addFields(   
+                    { name: result[2][1], value: result[2][2]},
+                )
+                .addFields(                 
+                    { name: result[3][1], value: result[3][2]},      
+                )
+                .addFields(              
+                    { name: result[4][1], value: result[4][2]},            
+                )
+                .addFields(
+                    { name: result[5][1], value: result[5][2]},                   
+                )
+                .addFields(   
+                    { name: result[6][1], value: result[6][2]},             
+                )
+                .setTimestamp()
+                .setFooter('Test-Bot-Cmd2-Kana');
+
+                message.channel.send(exampleEmbed);
+  
+                await browser.close()
+                
+              })
+            
         }else{
-            message.channel.send('invalid Command sier');
+            message.channel.send('Invalid command sier');    
         }
+
 
         
     }
